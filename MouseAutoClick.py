a4e25e05accd0592f2e9b22203b60d48b92ab069
theChiChen <chichen@chiichen.com>
2016-03-27 22:07:14 +0800

    讓滑鼠還可以移動

--- MouseAutoClick.py
+++ MouseAutoClick.py
@@ -2,18 +2,17 @@
 
 import win32api, win32con, win32gui, time
 
-# Get Mouse X Y
-x,y=win32gui.GetCursorPos()
-print("%s %s" %(x, y))
+def click():
+    # get mouse x, y
+    x,y=win32gui.GetCursorPos()
+#    print("%s %s" %(x, y))
 
-
-def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
 
 if __name__ == '__main__':
-	while True:
-		time.sleep(0.5)
-		click(x,y)
+    while True:
+        time.sleep(0.1)
+        click()
